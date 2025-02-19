5bc8ee88ef7961f4010b9bf909717f1085a990b6
roma6969 <roma.10402@mhs.amikomsolo.ac.id>
2025-02-19 19:12:25 +0700

    Create dzaki.py

---
+++ team/dzaki.py
@@ -0,0 +1,9 @@
+def perkenalan(nama,nim,peran):
+  print("hallo ! Perkenalkan , nama saya", nama)
+  print("Nim :", nim)
+  print("Peran saya yaitu : ", peran )
+if __name__ == "__main__":
+  nama = " Dzaki Satriatama "
+  nim = "2213010394"
+  peran = " Membuat tampilan login "
+  perkenalan(nama,nim,peran)
