814daa68a55dc041c981c9c5fb6a5fbb984401db
roma6969 <roma.10402@mhs.amikomsolo.ac.id>
2025-02-19 19:13:45 +0700

    Create farhannaufal.py

---
+++ team/farhannaufal.py
@@ -0,0 +1,9 @@
+def perkenalan(nama,nim,peran):
+  print("hallo ! Perkenalkan , nama saya", nama)
+  print("Nim :", nim)
+  print("Peran saya yaitu : ", peran )
+if __name__ == "__main__":
+  nama = " Farhan Naufal Mubarok "
+  nim = "2213010397"
+  peran = "Mengerjakan fitur tambah pesanan dan pesanan diterima"
+  perkenalan(nama,nim,peran)
