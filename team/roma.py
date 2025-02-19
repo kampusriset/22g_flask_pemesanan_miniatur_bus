1df57362e6939b334d7a9c078f14d83bf9e56cb5
roma6969 <roma.10402@mhs.amikomsolo.ac.id>
2025-02-19 19:10:45 +0700

    Create roma.py

---
+++ team/roma.py
@@ -0,0 +1,9 @@
+def perkenalan(nama,nim,peran):
+  print("hallo ! Perkenalkan , nama saya", nama)
+  print("Nim :", nim)
+  print("Peran saya yaitu : ", peran )
+if __name__ == "__main__":
+  nama = " Roma Arrizal Omar Syah "
+  nim = "2213010402"
+  peran = "Mengerjakan fitur pesanan diproses edit,delete,selesai"
+  perkenalan(nama,nim,peran)
